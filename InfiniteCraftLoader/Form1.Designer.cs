d9db62a069e98274ebc73357598034ba9ecab1b3
David Jongeneel <david.jongeneel@outlook.com>
2024-03-08 19:04:55 +0100

    Add project files.

---
+++ InfiniteCraftLoader/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace InfiniteCraftLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.newToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.openToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.saveToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.webView = new EO.WebBrowser.WebView();
+            this.webControl = new System.Windows.Forms.PictureBox();
+            this.toolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "JSON|*.json";
+            this.openFileDialog.Title = "Selecteer je Infinite Craft save file";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "json";
+            this.saveFileDialog.FileName = "infinitecraft-save";
+            this.saveFileDialog.Filter = "JSON|*.json";
+            this.saveFileDialog.Title = "Sla je Infinite Craft save file op";
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripButton,
+            this.openToolStripButton,
+            this.saveToolStripButton});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip.TabIndex = 0;
+            this.toolStrip.Text = "Menu";
+            // 
+            // newToolStripButton
+            // 
+            this.newToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("newToolStripButton.Image")));
+            this.newToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.newToolStripButton.Name = "newToolStripButton";
+            this.newToolStripButton.Size = new System.Drawing.Size(61, 22);
+            this.newToolStripButton.Text = "Nieuw";
+            this.newToolStripButton.Click += new System.EventHandler(this.newToolStripButton_Click);
+            // 
+            // openToolStripButton
+            // 
+            this.openToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripButton.Image")));
+            this.openToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripButton.Name = "openToolStripButton";
+            this.openToolStripButton.Size = new System.Drawing.Size(69, 22);
+            this.openToolStripButton.Text = "Openen";
+            this.openToolStripButton.Click += new System.EventHandler(this.openToolStripButton_Click);
+            // 
+            // saveToolStripButton
+            // 
+            this.saveToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripButton.Image")));
+            this.saveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveToolStripButton.Name = "saveToolStripButton";
+            this.saveToolStripButton.Size = new System.Drawing.Size(70, 22);
+            this.saveToolStripButton.Text = "Opslaan";
+            this.saveToolStripButton.Click += new System.EventHandler(this.saveToolStripButton_Click);
+            // 
+            // webControl
+            // 
+            this.webControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webControl.Location = new System.Drawing.Point(0, 25);
+            this.webControl.Name = "webControl";
+            this.webControl.Size = new System.Drawing.Size(800, 425);
+            this.webControl.TabIndex = 1;
+            this.webControl.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webControl);
+            this.Controls.Add(this.toolStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Infinite Craft Loader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webControl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton newToolStripButton;
+        private System.Windows.Forms.ToolStripButton openToolStripButton;
+        private System.Windows.Forms.ToolStripButton saveToolStripButton;
+        private EO.WebBrowser.WebView webView;
+        private System.Windows.Forms.PictureBox webControl;
+    }
+}
+
---
+++ InfiniteCraftLoader/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace InfiniteCraftLoader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize the web browser
+            webView.Create(webControl.Handle);
+            webView.Url = "https://neal.fun/infinite-craft/";
+        }
+
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            // remove "infinite-craft-data" in Local Storage
+            webView.EvalScript("localStorage.removeItem('infinite-craft-data');");
+
+            // refresh the page
+            webView.Reload();
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Read the json file
+                string json = System.IO.File.ReadAllText(openFileDialog.FileName);
+
+                // Set the json data to "infinite-craft-data" in Local Storage
+                webView.EvalScript("localStorage.setItem('infinite-craft-data', '" + json + "');");
+
+                // refresh the page
+                webView.Reload();
+            }   
+
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the json data from "infinite-craft-data" in Local Storage
+                string json = webView.EvalScript("localStorage.getItem('infinite-craft-data');").ToString();
+
+                // Write the json data to the file
+                File.WriteAllText(saveFileDialog.FileName, json);
+            }
+        }
+    }
+}
